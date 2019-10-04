3e1a8e045c932286ced1917a62a48b79fec1b78a
unknown <lucas.ferreira@primeup.com.br>
2019-10-04 11:55:46 -0300

    teste deploy

--- src/main/java/com/devweb/camisaslegais/HelloAppEngine.java
+++ src/main/java/com/devweb/camisaslegais/HelloAppEngine.java
@@ -20,7 +20,7 @@ public class HelloAppEngine extends HttpServlet {
     Properties properties = System.getProperties();
 
     response.setContentType("text/plain");
-    response.getWriter().println("Hello App Engine - Standard using "
+    response.getWriter().println("ROGERINHO BRABO Hello App Engine - Standard using "
         + SystemProperty.version.get() + " Java " + properties.get("java.specification.version"));
   }
 
